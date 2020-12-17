77df7462c386deb2bd9aff49d7dde80155ae290f
Orita <wuttichai3113@gmail.com>
2020-12-17 23:53:27 +0700

    Move to simple concept

--- website/script.js
+++ website/script.js
@@ -1,37 +1,19 @@
-document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-    const dropZoneElement = inputElement.closest(".drop-zone");
+const myForm = document.getElementById("myForm")
+const inpFile = document.getElementById("inpFile")
 
-    dropZoneElement.addEventListener("dragover", e => {
-        e.preventDefault();
-        dropZoneElement.classList.add("drop-zone--over");
+myForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const endpoint = 'http://localhost:8080/upload'
+    const formData = new FormData()
+
+    // inpFile come form name="inpFile"
+    formData.append("inpFile", inpFile.files[0])
+
+    fetch(endpoint, {
+        method: "POST",
+        body: formData,
+        mode: 'no-cors',
     })
-
-
-    let dragEvents = ['dragleave', 'dragend'];
-    dragEvents.forEach(type => {
-        dropZoneElement.addEventListener(type, e => {
-            dropZoneElement.classList.remove("drop-zone--over");
-        });
-    });
-
-
-    dropZoneElement.addEventListener("drop", e => {
-        e.preventDefault();
-
-        if (e.dataTransfer.files.length) {
-            inputElement.files = e.dataTransfer.files;
-            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-
-            dropZoneElement.classList.remove("drop-zone--over");
-        }
-    });
-});
-
-function updateThumbnail(dropZoneElement, file) {
-    let thumnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-    // First time - there is no thumbnail element, so lets create it
-    thumnailElement = document.createElement("div");
-    thumnailElement.classList.add("drop-zone__thumb");
-    dropZoneElement.appendChild(thumnailElement);
-}
+    .catch(console.error)
+})
